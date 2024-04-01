b295802acc20e20275de95edfdace79b4f34c190
Liideli <roope.kasperi.laine@gmail.com>
2024-04-01 13:03:35 +0300

    usercontroller

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,14 +3,14 @@ import express from 'express';
 import userRoute from './routes/userRoute';
 import catRoute from './routes/catRoute';
 import authRoute from './routes/authRoute';
-import MessageResponse from '../interfaces/MessageResponse';
+//import MessageResponse from '../interfaces/MessageResponse';
 import passport from 'passport';
 
 const router = express.Router();
 
 router.use(passport.initialize());
 
-router.get<{}, MessageResponse>('/', (req, res) => {
+router.get('/', (req, res) => {
   res.json({
     message: 'routes: auth, user, cat',
   });
